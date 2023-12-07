61c84f6e8cdf79ba326458157c5c0493094097d5
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2023-12-07 10:17:07 +0100

    Remove explicit testing step from GitHub Action

--- src/test/java/at/aau/ase/workouttrackeruserservice/WorkoutTrackerUserServiceApplicationTests.java
+++ src/test/java/at/aau/ase/workouttrackeruserservice/WorkoutTrackerUserServiceApplicationTests.java
@@ -4,4 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WorkoutTrackerUserServiceApplicationTests {}
+class WorkoutTrackerUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
