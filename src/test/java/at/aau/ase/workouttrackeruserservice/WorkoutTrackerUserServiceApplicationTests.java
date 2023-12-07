f07aa8a62ab9464f41cc8ed7bd7bfaae0d140770
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2023-12-07 10:14:53 +0100

    Remove context load test, because of missing DB

--- src/test/java/at/aau/ase/workouttrackeruserservice/WorkoutTrackerUserServiceApplicationTests.java
+++ src/test/java/at/aau/ase/workouttrackeruserservice/WorkoutTrackerUserServiceApplicationTests.java
@@ -4,9 +4,4 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WorkoutTrackerUserServiceApplicationTests {
-
-  @Test
-  void contextLoads() {}
-
-}
+class WorkoutTrackerUserServiceApplicationTests {}
